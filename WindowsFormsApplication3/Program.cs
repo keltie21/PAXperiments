fd6b65674ad7d12e2ac89114f7eede194d978489
Patrick <tundemoran@gmail.com>
2016-02-03 10:59:29 -0500

    ftp successfuly transfers

--- WindowsFormsApplication3/Program.cs
+++ WindowsFormsApplication3/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Net;
+using System.Text;
+
 namespace WindowsFormsApplication3
 {
     static class Program
@@ -35,6 +38,37 @@ namespace WindowsFormsApplication3
                 }
             
             }
+
+            // FTP setup
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(@"ftp://70.32.92.206/test/newfile.txt");
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+
+            request.Credentials = new NetworkCredential("paxftp", @"$\/]=2m%[<9%]4B");
+
+            // later this will be the images...
+            StreamReader sourceStream = new StreamReader(@"C:\Users\Tunde\Desktop\makelab\WindowsFormsApplication3\WindowsFormsApplication3\testfile.txt");
+            byte[] fileContents = Encoding.UTF8.GetBytes(sourceStream.ReadToEnd());
+            sourceStream.Close();
+            request.ContentLength = fileContents.Length;
+
+            try
+            {
+                Stream requestStream = request.GetRequestStream();
+                requestStream.Write(fileContents, 0, fileContents.Length);
+                requestStream.Close();
+
+                FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+                Console.WriteLine("Upload'd  --- stat {0}", response.StatusDescription);
+
+                response.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("FAIL {0}", e.Message);
+            }
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmSynch());
