a37cc155fc8f88ff29f486dca944a6bc065ff882
Patrick <tundemoran@gmail.com>
2016-02-03 15:57:27 -0500

    directory movement

---
+++ graffiti/WindowsFormsApplication3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace WindowsFormsApplication3
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            string[] args = Environment.GetCommandLineArgs();
+            
+           
+
+            ///
+            /// Taskbar setup
+            ///
+
+            ///
+            /// FTP setup
+            /// 
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmSynch());
+        }
+    }
+}
